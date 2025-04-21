67a741686500c2ed4d854f699098e78bee291014
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-04-21 14:24:08 +0530

    trainingcontroller and trainingroutes bug fixes

--- Server/routes/trainingRoutes.js
+++ Server/routes/trainingRoutes.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const trainingRoutes = express.Router();
-const { addAthlete, getAllAthletes, getAthleteById  } = require('../controllers/trainingController');
-
-trainingRoutes.post('/addAthlete', addAthlete);
-trainingRoutes.get('/', getAllAthletes);
-trainingRoutes.get('/:id', getAthleteById);   
 
 
 module.exports = trainingRoutes;
