9ca43b0c1ebfb420993a2b8cdec03bcd43625b28
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-04-22 10:12:54 +0530

    db read and write

--- Server/controllers/athleteController.js
+++ Server/controllers/athleteController.js
@@ -43,4 +43,17 @@ const getAthleteById = async (req, res) => {
     }
 };
 
-module.exports = { addAthlete, getAllAthletes, getAthleteById  };
+const deleteAthlete = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedAthlete = await Athlete.findByIdAndDelete(id);
+        if (!deletedAthlete) {
+            return res.status(404).json({ message: "Athlete not found" });
+        }
+        res.status(200).json({ message: "Athlete deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+};
+
+module.exports = { addAthlete, getAllAthletes, getAthleteById, deleteAthlete };
