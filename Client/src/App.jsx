f9357382e7baeba8ce154e6a4b15a0a703ab1d5b
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-08-12 22:10:35 +0530

    SignUp page done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 // import HomePage from './pages/HomePage/HomePage'
+import SignUp from './pages/Register/SignUp'
 
 const App = () => {
   return (
     // <HomePage/>
-    <Sidebar/>
+    // <Sidebar/>
+    <SignUp/>
   )
 }
 
